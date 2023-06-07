6801839b4ffc094c310c9b43ee3a4c91fe3e21e2
Ngo Dang Khoa <131435439+NgoDangKhoauit@users.noreply.github.com>
2023-06-07 08:13:56 +0700

    Add files via upload

--- frontend/renderer.py
+++ frontend/renderer.py
@@ -18,7 +18,7 @@ class Renderer():
 
         return pygame.font.Font("font/font.ttf", size)
     
-    def draw_board(screen, board_size, cell_size):
+    def draw_board(screen, board_size=15, cell_size=45):
         for i in range(1, board_size + 1):
             pygame.draw.line(screen, palette.BLACK, [cell_size * i, cell_size], [cell_size * i, cell_size*15], 2)
             pygame.draw.line(screen, palette.BLACK, [cell_size, cell_size * i], [cell_size*15, cell_size * i], 2)
@@ -62,49 +62,47 @@ class Renderer():
         surf.blit(circle_surface, circle_surface.get_rect(center=center),
                   special_flags=pygame.BLEND_PREMULTIPLIED)
     
-    def draw_score(self, player1_score, player2_score, screen, board_size=15, cell_size=45):
+    def draw_score(self, player1_score, player2_score, screen,current_player=1, board_size=15, cell_size=45):
         
         y_center = screen.get_height() // 2
         
         p1_center = cell_size * board_size + cell_size
         p1_score_center = cell_size * board_size + (screen.get_width() - cell_size * board_size) // 2
-        p1_color = palette.BLACK
+        if current_player == 1:
+            p1_color = palette.RED
+        else:
+            p1_color = palette.BLACK
         p1_text = self.get_font("Roboto-Medium", 20).render("Player 1", True, p1_color)
         screen.blit(p1_text, (p1_center, y_center - cell_size * 2))
 
         # Draw Player 1 score
         score_font = pygame.font.Font(None, 45)
-        p1_score_text = score_font.render(str(player1_score), True, p1_color)
+        p1_score_text = score_font.render(str(player1_score), True, palette.BLACK)
+        p1_rect = pygame.Rect(45*17 + 5, 45*7 - 13, 50, 50)
+        screen.fill(palette.BOARD_COLOR, p1_rect)
         screen.blit(p1_score_text, (p1_score_center, y_center - cell_size - 1))
 
         # Draw "Player 2" text
-        p2_color = palette.WHITE
+        if current_player == 2:
+            p2_color = palette.RED
+        else:
+            p2_color = palette.WHITE
         p2_text = self.get_font("Roboto-Medium", 20).render("Player 2", True, p2_color)
         screen.blit(p2_text, (p1_center, y_center + cell_size * 2 - 20))
 
         # Draw Player 2 score
-        p2_score_text = score_font.render(str(player2_score), True, p2_color)
+        p2_score_text = score_font.render(str(player2_score), True, palette.WHITE)
         screen.blit(p2_score_text, (p1_score_center, y_center + cell_size - 20))
         
-    
-
-# screen_size = (700, 700)
-# screen = pygame.display.set_mode(screen_size)
-
-# pygame.init()        
-
-
-# game_state = GameState(Grid(15, cell_size=45))
-
-# while True:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             pygame.quit()
-#             sys.exit()
-
-#     # Render the game state
-#     a.render(game_state)
-
-#     pygame.display.update()
-
+        pygame.display.flip()
         
+    def display_time(self, x, y, time_per_turn, start_time, screen):
+        elapsed_time = pygame.time.get_ticks() - start_time
+        remaining_time = max(0, time_per_turn - elapsed_time)
+        seconds = int(remaining_time / 1000)
+        minutes = seconds // 60
+        seconds = seconds % 60
+        time_string = f"{minutes:02d}:{seconds:02d}"
+        time_surf = pygame.font.Font(None, 30).render(time_string, True, palette.BLACK)
+        time_rect = time_surf.get_rect(center=(x, y))
+        screen.blit(time_surf, time_rect)
